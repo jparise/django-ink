f0e2ab9cfbae7a62a8459d4cb21fbd3df94dfd75
Jon Parise <jon@indelible.org>
2008-08-09 16:12:02 -0700

    Adding a distutils-based setup.py script.

---
+++ setup.py
@@ -0,0 +1,69 @@
+"""
+Based on Django's own ``setup.py'' script.
+"""
+
+import os
+from distutils.command.install import INSTALL_SCHEMES
+from distutils.core import setup
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neurtral ay.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+version = __import__('ink').VERSION
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages and data_files.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+ink_dir = os.path.join(root_dir, 'ink')
+pieces = fullsplit(root_dir)
+if pieces[-1] == '':
+    len_root_dir = len(pieces) - 1
+else:
+    len_root_dir = len(pieces)
+
+for dirpath, dirnames, filenames in os.walk(ink_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+        if '__init__.py' in filenames:
+            packages.append('.'.join(fullsplit(dirpath)[len_root_dir:]))
+        elif filenames:
+            data_files.append([dirpath,
+                               [os.path.join(dirpath, f) for f in filenames]])
+
+setup(
+    name = 'ink',
+    version = '%d.%d' % version,
+    description = 'Blogging application for Django',
+    author = 'Jon Parise',
+    author_email = 'jon@indelible.org',
+    url = 'http://www.indelible.org/projects/ink/',
+    packages = packages,
+    data_files = data_files,
+    classifiers = ['Development Status :: 2 - Pre-Alpha',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content'],
+)
