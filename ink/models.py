dd8fef8d72573557128b1be5de8c1f530c3d4034
Jon Parise <jon@indelible.org>
2008-08-10 23:26:38 -0700

    get_absolute_url() needs to be decorated as a permalink.

--- ink/models.py
+++ ink/models.py
@@ -47,6 +47,7 @@ class Entry(models.Model):
     def __unicode__(self):
         return self.title
 
+    @models.permalink
     def get_absolute_url(self):
         return ('ink_entry_detail', (),
                     {'year': self.pub_date.strftime('%Y'),
