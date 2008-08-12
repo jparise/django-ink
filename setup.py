0f7629afc12f51a7e0050b0aa4188c0078188e09
Jon Parise <jon@indelible.org>
2008-08-11 22:33:39 -0700

    Simplifying the setup.py script and extending the version tuple to include the release type.
    I'll add true setuptools support some time in the future.  That will give us
    dependency support so we can properly require, e.g. the tagging application.

--- setup.py
+++ setup.py
@@ -1,62 +1,20 @@
-"""
-Based on Django's own ``setup.py'' script.
-"""
-
-import os
-from distutils.command.install import INSTALL_SCHEMES
 from distutils.core import setup
 
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral ay.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-version = __import__('ink').VERSION
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages and data_files.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-ink_dir = os.path.join(root_dir, 'ink')
-pieces = fullsplit(root_dir)
-if pieces[-1] == '':
-    len_root_dir = len(pieces) - 1
+# Dynamically calculate the version based on ink.VERSION.
+version_tuple = __import__('ink').VERSION
+if version_tuple[2] is not None:
+    version = '%d.%d_%s' % version_tuple
 else:
-    len_root_dir = len(pieces)
-
-for dirpath, dirnames, filenames in os.walk(ink_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)[len_root_dir:]))
-    elif filenames:
-        data_files.append([dirpath,
-                          [os.path.join(dirpath, f) for f in filenames]])
+    version = '%d.%d' % version_tuple[:2]
 
 setup(
     name = 'ink',
-    version = '%d.%d' % version,
+    version = version,
     description = 'Blogging application for Django',
     author = 'Jon Parise',
     author_email = 'jon@indelible.org',
     url = 'http://www.indelible.org/projects/ink/',
-    packages = packages,
-    data_files = data_files,
+    packages = ['ink', 'ink.templatetags'],
     classifiers = ['Development Status :: 2 - Pre-Alpha',
                    'Environment :: Web Environment',
                    'Framework :: Django',
